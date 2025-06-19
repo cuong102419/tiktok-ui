2df574a4e7f9c130cb9de3f0b18c6f0537be1765
cuongpmph45042 <cuongmanh1024@gmail.com>
2025-06-19 23:40:00 +0700

    Tái cấu trúc và tối ưu code #1

--- src/layouts/components/Search/index.js
+++ src/layouts/components/Search/index.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames/bind';
 import HeadlessTippy from '@tippyjs/react/headless';
 
-import * as searchServices from '~/apiServices/searchServices';
+import * as searchServices from '~/services/searchService';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { Clear, Loading, SearchIcon } from '~/components/Icons';
@@ -63,7 +63,7 @@ function Search() {
     };
 
     return (
-        // Using a wrapper <div> or <span> tag around the reference element solves 
+        // Using a wrapper <div> or <span> tag around the reference element solves
         // this by creating a new parentNode context.
         <div>
             <HeadlessTippy
